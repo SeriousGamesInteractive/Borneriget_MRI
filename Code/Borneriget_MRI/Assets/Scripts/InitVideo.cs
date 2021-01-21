fd7dd4124fc1775466645b1dffa02628ac3386cc
Thomas Gravgaard <tg@sentio.net>
2021-01-21 15:09:24 +0100

    Restored a lot of files from LFS

--- Code/Borneriget_MRI/Assets/Scripts/InitVideo.cs
+++ Code/Borneriget_MRI/Assets/Scripts/InitVideo.cs
@@ -1,3 +1,103 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8dbfd0ad5d8aadbc30a20cdc8480b0745fd8ee07769ac7b4c702e0c1bd113c2f
-size 2296
+using Google.XR.Cardboard;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.XR.Management;
+
+public class InitVideo : MonoBehaviour
+{
+    public VideoPlayer Video;
+    public Button StartButton;
+    public TMPro.TextMeshProUGUI Label;
+
+    private bool vrPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartButton.onClick.AddListener(ButtonClicked);
+    }
+
+    private void ButtonClicked()
+    {
+        if (vrPlaying)
+        {
+            StopXR();
+        }
+        else
+        {
+            StartCoroutine(InitializeXRAndStart());
+        }
+    }
+
+    private void Log(string message, bool error = false)
+    {
+        Label.text = message;
+        Label.color = (error) ? Color.red : Color.white;
+        if (error)
+        {
+            Debug.LogError(message);
+        }
+        else
+        {
+            Debug.Log(message);
+        }
+    }
+
+    private IEnumerator InitializeXRAndStart()
+    {
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Log("Initializing XR Failed.");
+        }
+        else
+        {
+            Log("XR initialized.");
+            if (!Api.HasDeviceParams())
+            {
+                Api.ScanDeviceParams();
+            }
+            Log("Starting XR...");
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+            Log("XR started.");
+            Video.Play();
+            vrPlaying = true;
+        }
+    }
+
+    private void StopXR()
+    {
+        Log("Stopping XR...");
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        Log("XR stopped.");
+
+        Log("Deinitializing XR...");
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        Log("XR deinitialized.");
+
+        Video.Stop();
+    }
+
+    public void Update()
+    {
+        if (vrPlaying)
+        {
+            if (Api.IsCloseButtonPressed)
+            {
+                StopXR();
+            }
+
+            if (Api.IsGearButtonPressed)
+            {
+                Api.ScanDeviceParams();
+            }
+
+            Api.UpdateScreenParams();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a8684b567c896be33c37349fc66df5e63f7c06e381cd151070ef18e18315af6d
-size 16
+# Borneriget_MRI
